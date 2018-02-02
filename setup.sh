0a13b69ed5d0814d9c312c877a250e24c6669d5e
Laurent cetinsoy <laurent.cetinsoy@gmail.com>
2018-02-02 18:34:01 +0100

    adding initial file

---
+++ setup.sh
@@ -0,0 +1,49 @@
+
+install_composer(){
+
+	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+	php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+	php composer-setup.php
+	php -r "unlink('composer-setup.php');"
+	mkdir ~/bin
+	mv composer.phar ~/bin/composer
+
+}
+
+install_php(){
+
+     sudo add-apt-repository ppa:ondrej/php
+     sudo apt-get update
+
+     sudo apt install php7.1-cli php7.1-common php7.1-curl php7.1-fpm php7.1-gd php7.1-imap php7.1-intl php7.1-json php7.1-mbstring php7.1-mysql php7.1-opcache php7.1-readline php7.1-xml php7.1-zip
+
+}
+
+install_docker_ce(){
+
+	sudo apt-get install \
+	    apt-transport-https \
+	    ca-certificates \
+	    curl \
+	    software-properties-common
+
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+	sudo add-apt-repository \
+	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	   $(lsb_release -cs) \
+	   stable"
+
+	sudo apt-get update
+	sudo apt-get install docker-ce
+        sudo usermod -a -G docker $USER
+}
+
+install_docker_compose(){
+
+    #need docker raw
+    
+    sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+    sudo chmod +x /usr/local/bin/docker-compose
+}
