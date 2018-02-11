2e134aafd1789c8f751315231b7072d765450136
Laurent Cetinsoy <laurent.cetinsoy@gmail.com>
2018-02-11 22:32:14 +0100

    adding desktop specific installs

---
+++ desktop_setup.sh
@@ -0,0 +1,14 @@
+sudo apt install -y redshift-gtk
+sudo apt install -y vim
+sudo apt install -y python3-pip python-pip
+pip install fabric
+
+pip install jupyter numpy scipy scikit-learn pandas matplotlib
+
+sudo apt-add-repository ppa:blahota/texstudio && sudo apt install -y texstudio
+sudo apt install -y texlive texlive-science texlive-lang-french texlive-extra
+
+
+
+
+#yarn ..
